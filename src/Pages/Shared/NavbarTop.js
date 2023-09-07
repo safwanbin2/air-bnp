010d71142e5ac28f8e14bda3bdaf0718e57798f0
safwanbin2 <safwanridwan321@gmail.com>
2023-09-07 23:56:02 +0600

    2nd sub - 2

--- src/Pages/Shared/NavbarTop.js
+++ src/Pages/Shared/NavbarTop.js
@@ -105,7 +105,7 @@ const NavbarTop = () => {
         <>
             <div ref={menuRef} className={`${isOpen ? "h-40 absolute top-0 left-0 w-full bg-white shadow" : "h-20"} flex justify-center items-center border-b z-10 transition-all duration-200`}>
                 {/* while isOpen is false */}
-                <div onClick={() => setIsOpen(!isOpen)} className={`w-[28%] grid-cols-3 px-4 py-2 rounded-3xl border shadow-md hover:shadow-lg font-semibold items-center gap-1 cursor-pointer  ${isOpen ? "hidden" : "grid"}`}>
+                <div onClick={() => setIsOpen(!isOpen)} className={`w-11/12 md:w-[28%] grid-cols-3 px-4 py-2 rounded-3xl border shadow-md hover:shadow-lg font-semibold items-center gap-1 cursor-pointer  ${isOpen ? "hidden" : "grid"}`}>
                     <p className='text-sm text-center'>{location ? location : "Anywhere"}</p>
                     <p className='text-sm border-s border-e text-center'>{(d1 || d2) ? `${d1} - ${d2}` : "Anyweek"}</p>
                     <p className='whitespace-nowrap text-sm flex justify-center items-center gap-2 text-center'>{capacity ? capacity : "Add"} guest<span className='bg-primary flex justify-center items-center rounded-full w-8 h-8'><svg className='h-5 w-5 fill-white' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" id="search"><g data-name="Layer 2"><path d="m20.71 19.29-3.4-3.39A7.92 7.92 0 0 0 19 11a8 8 0 1 0-8 8 7.92 7.92 0 0 0 4.9-1.69l3.39 3.4a1 1 0 0 0 1.42 0 1 1 0 0 0 0-1.42zM5 11a6 6 0 1 1 6 6 6 6 0 0 1-6-6z" data-name="search"></path></g></svg></span></p>
@@ -119,7 +119,7 @@ const NavbarTop = () => {
                         <Link className=' py-2'>Online Experiences</Link>
                     </div>
                     <div className='h-20 py-2'>
-                        <div className='h-full min-w-[620px] rounded-full bg-base-200 flex justify-center items-center shadow-md relative'>
+                        <div className='h-full w-full md:min-w-[620px] rounded-full bg-base-200 flex justify-center items-center shadow-md relative'>
                             <div className='grid justify-center items-center h-full ' style={{ gridTemplateColumns: "4fr 2fr 3fr" }}>
                                 <button onClick={handleIsSearchOpen} className={`h-full flex justify-center flex-col rounded-full ps-6 ${isSearchOpen && "bg-white shadow-2xl"}`}>
                                     <p className='text-sm font-semibold'>Search</p>
@@ -195,8 +195,8 @@ const NavbarTop = () => {
             <div className={`${isOpen ? "fixed top-0 left-0 min-h-screen min-w-full bg-[#00000040]" : "hidden"} `}>
 
             </div>
-            <button onClick={handleRefresh} className='absolute top-6 left-14 z-20 text-primary text-2xl font-semibold'>airbnp</button>
-            <button className='absolute top-7 right-14 z-20 '>
+            <button onClick={handleRefresh} className='hidden md-absolute top-6 left-14 z-20 text-primary text-2xl font-semibold'>airbnp</button>
+            <button className='hidden md-absolute top-7 right-14 z-20 '>
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" aria-hidden="true" role="presentation" focusable="false" 
                 className={`block h-5 w-5 fill-current`} ><path d="M8 .25a7.77 7.77 0 0 1 7.75 7.78 7.75 7.75 0 0 1-7.52 7.72h-.25A7.75 7.75 0 0 1 .25 8.24v-.25A7.75 7.75 0 0 1 8 .25zm1.95 8.5h-3.9c.15 2.9 1.17 5.34 1.88 5.5H8c.68 0 1.72-2.37 1.93-5.23zm4.26 0h-2.76c-.09 1.96-.53 3.78-1.18 5.08A6.26 6.26 0 0 0 14.17 9zm-9.67 0H1.8a6.26 6.26 0 0 0 3.94 5.08 12.59 12.59 0 0 1-1.16-4.7l-.03-.38zm1.2-6.58-.12.05a6.26 6.26 0 0 0-3.83 5.03h2.75c.09-1.83.48-3.54 1.06-4.81zm2.25-.42c-.7 0-1.78 2.51-1.94 5.5h3.9c-.15-2.9-1.18-5.34-1.89-5.5h-.07zm2.28.43.03.05a12.95 12.95 0 0 1 1.15 5.02h2.75a6.28 6.28 0 0 0-3.93-5.07z"></path></svg>
             </button>
